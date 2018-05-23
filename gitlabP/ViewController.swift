b94edf2dbd1d52074c421bb17d581de55ba79cd3
SotheaNem <nemsothe13@gmail.com>
2018-05-23 11:02:43 +0700

    Initial Commit

---
+++ gitlabP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitlabP
+//
+//  Created by SotheaNem on 5/23/18.
+//  Copyright © 2018 Sothea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
